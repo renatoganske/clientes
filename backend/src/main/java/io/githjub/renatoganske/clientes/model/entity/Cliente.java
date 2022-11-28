f8d112dca23fc14633d4895991eced7671ceb0a9
Renato Ganske Junior <renatoganske.jr@gmail.com>
2022-11-27 23:58:20 -0300

    feat: estabelecida conexão com o banco de dados test: elaborado teste de conexão com banco de dados por meio de CommandLineRunner

--- backend/src/main/java/io/githjub/renatoganske/clientes/model/entity/Cliente.java
+++ backend/src/main/java/io/githjub/renatoganske/clientes/model/entity/Cliente.java
@@ -1,12 +1,18 @@
 package io.githjub.renatoganske.clientes.model.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,4 +26,9 @@ public class Cliente {
 
     @Column(name = "data_cadastro")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist(){
+        setDataCadastro(LocalDate.now());
+    }
 }
