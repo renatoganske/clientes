8b3810cb230f4a85d24f0db7430b853ecebe1c08
Renato Ganske Junior <renatoganske.jr@gmail.com>
2022-11-27 23:20:33 -0300

    feat: Mapeada a entidade cliente

---
+++ backend/src/main/java/io/githjub/renatoganske/clientes/model/entity/Cliente.java
@@ -0,0 +1,21 @@
+package io.githjub.renatoganske.clientes.model.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+}
