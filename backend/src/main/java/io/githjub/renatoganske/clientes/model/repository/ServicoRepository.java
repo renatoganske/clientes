c68cd05e6fe4ab90bc68f6b157ebac78617a5548
Renato Ganske Junior <renatoganske.jr@gmail.com>
2022-11-27 23:33:50 -0300

    feat: Criados os repositórios de Cliente e de Serviço

---
+++ backend/src/main/java/io/githjub/renatoganske/clientes/model/repository/ServicoRepository.java
@@ -0,0 +1,7 @@
+package io.githjub.renatoganske.clientes.model.repository;
+
+import io.githjub.renatoganske.clientes.model.entity.Servico;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServicoRepository extends JpaRepository<Servico, Integer> {
+}
