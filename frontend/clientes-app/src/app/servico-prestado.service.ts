ed11d4c16a3f5d94434a43ab33baf31134bfa88c
Renato Ganske <renato.j@lyncas.net>
2023-01-30 17:07:16 -0300

    feat: implementado formulário de pesquisa de serviços prestados

--- frontend/clientes-app/src/app/servico-prestado.service.ts
+++ frontend/clientes-app/src/app/servico-prestado.service.ts
@@ -1,6 +1,7 @@
+import { ServicoPrestadoBusca } from './servico-prestado/servico-prestado-lista/servicoPrestadoBusca';
 
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { ServicoPrestado } from './servico-prestado/servicoPrestado';
 import { Observable } from 'rxjs';
 import { environment } from '../environments/environment'
@@ -19,4 +20,15 @@ export class ServicoPrestadoService {
       this.apiURL, servicoPrestado
       );
   }
+
+  buscar(nome: string, mes: number) : Observable<ServicoPrestadoBusca[]>{
+
+    const httpParams = new HttpParams()
+      .set("nome", nome)
+      .set("mes", mes ?  mes.toString() : '');
+
+    const url = this.apiURL + "?" + httpParams.toString();
+    return this.http.get<any>(url);
+  }
+
 }
